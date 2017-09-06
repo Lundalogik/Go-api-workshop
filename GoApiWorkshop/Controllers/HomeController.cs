fe95b26459004b614f49f5401ab663c1e24b3e95
Ahmad Game <ahmad.game@lundalogik.se>
2017-09-06 12:01:37 +0200

    Post some stuff

--- GoApiWorkshop/Controllers/HomeController.cs
+++ GoApiWorkshop/Controllers/HomeController.cs
@@ -24,8 +24,53 @@ namespace GoApiWorkshop.Controllers
         [HttpPost]
         public IActionResult Contact(ContactModel model)
         {
-            ViewData["Title"] = "Contact us!";
+            if (ModelState.IsValid)
+            {
+                var json = JsonConvert.SerializeObject(new
+                {
+                    name = "Contact request",
+                    strength = "90",
+                    note = model.Message,
+                    organization = new
+                    {
+                        name = model.CompanyName
+                    },
+                    person = new
+                    {
+                        firstName = model.PersonFirstName,
+                        lastName = model.PersonLastName
+                    }
+                }, Formatting.Indented);
 
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var url = "https://api.staging.lime-go.com/v1/signal";
+                var apiKey = "96173b00-731c-4266-bdd9-708e0aea2f63";
+                var httpClient = new HttpClient();
+                var response = httpClient.PostAsync($"{url}?{apiKey}", content);
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    ModelState.Clear();
+                    ViewData["Message"] = "Thank you for contacting us!";
+                }
+                else
+                {
+                    ModelState.Clear();
+                    ViewData["Message"] = $"{response.Result.StatusCode} - {response.Result.ReasonPhrase}";
+                }
+            }
+            else
+            {
+                ViewData["Message"] = null;
+            }
+
+            ViewData["Title"] = "Another contact form";
+
+            return View();
+        }
+
+        public IActionResult AnotherContact()
+        {
+            ViewData["Title"] = "Contact us!";
             return View();
         }
 
