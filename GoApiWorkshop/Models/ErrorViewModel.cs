c646a5c514cd1fefdc91d733f714163f36690c47
Ahmad Game <ahmad.game@lundalogik.se>
2017-09-06 10:43:10 +0200

    Add project files.

---
+++ GoApiWorkshop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoApiWorkshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
